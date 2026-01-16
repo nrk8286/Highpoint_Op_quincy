e239520969fe1f6ed07abb96335d5d587fdca1ba
Nicholas Kelly <nrk8286@gmail.com>
2026-01-16 17:59:31 +0000

    Application error: a client-side exception has occurred while loading st

--- src/firebase/error-emitter.ts
+++ src/firebase/error-emitter.ts
@@ -1,6 +1,36 @@
-import { EventEmitter } from 'events';
+'use client';
 
-// This is a browser-safe event emitter.
-class SafeEventEmitter extends EventEmitter {}
+import type { FirestorePermissionError } from './errors';
 
-export const errorEmitter = new SafeEventEmitter();
+type Listener = (error: FirestorePermissionError) => void;
+
+type EventName = 'permission-error';
+
+class ErrorEmitter {
+  private listeners: { [key: string]: Listener[] } = {};
+
+  on(eventName: EventName, listener: Listener) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName].push(listener);
+  }
+
+  off(eventName: EventName, listener: Listener) {
+    if (!this.listeners[eventName]) {
+      return;
+    }
+    this.listeners[eventName] = this.listeners[eventName].filter(
+      (l) => l !== listener
+    );
+  }
+
+  emit(eventName: EventName, error: FirestorePermissionError) {
+    if (!this.listeners[eventName]) {
+      return;
+    }
+    this.listeners[eventName].forEach((listener) => listener(error));
+  }
+}
+
+export const errorEmitter = new ErrorEmitter();
